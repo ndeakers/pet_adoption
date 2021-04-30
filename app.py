028cc6cb436531aea243c39efa288cd18d310ba7
Nate Deakers <ndeakers@gmail.com>
2021-04-30 12:12:07 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, redirect, render_template, flash
+from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
